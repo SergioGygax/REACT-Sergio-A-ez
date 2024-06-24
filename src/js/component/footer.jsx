4813a4de4dfb03d402f3b718d632f68b7ac81313
Sergio Añez <160168084+SergioGygax@users.noreply.github.com>
2024-06-24 18:27:26 +0000

    Landing page terminada

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = ()=>{
     return (
         <div className="bg-dark text-white text-center py-3 mt-4 ">
-        &copy; SergiDev | 2024. All rights reserved.
+        &copy; SergiDev | Copyright your website 2024
       </div>
     )
 }
